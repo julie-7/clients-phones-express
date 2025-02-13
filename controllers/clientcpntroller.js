f5fe5c7036cb50f93e71e2af99d99ecca06857e5
Julie <julie23@gmail.com>
2025-02-13 16:23:42 -0600

    my next commit

---
+++ controllers/clientcpntroller.js
@@ -0,0 +1,29 @@
+import {db} from "../db/cn.js"
+
+export const getclients = async(req,res)=>{
+
+    const sql = `select * from clients `
+    const result = await db.query(sql)
+    res.json(result)
+}
+
+export const postclients = async (req,res)=>{
+    const temp = req.body
+    const params = (temp.id,temp.name,temp.email)
+    const sql = `insert into clients(id,name,email) values ($1,$2,$3) return id_client`
+    const result = await db.query(sql,params)
+    res.json(result)
+}
+
+export const putClients = async(req,res)=>{
+const temp = req.body
+const id_client = req.params.id
+const params = [temp.id,temp.name,temp.email, id_client]
+const sql = `update clients
+                set id = $1,
+                    email = $2,
+                    name = $3
+                    where id_client = $4`
+    const result = await db.query(sql,params)
+            res.json({message:"object modified"})
+}
--- project-routes/clientRoutes.js
+++ project-routes/clientRoutes.js
@@ -1,16 +1,7 @@
-function App() {
-const baseUrl = import.meta.env.VITE_BASE_URL
-const endPoint = "Clients"
+import express from from `express`
+export const client = express()
+import { getclients,postclients,putClients, } from "../controllers/clientcpntroller"
 
-const getClients = async ()=>{
-
-    const url= `${baseUrl}${endPoint}`
-    console.log(url)
-    const result= await fetch(url)
-    const data = result.json()
-    console.log(data)
-}
-useEffect(()=>{
-getClients()
-},[])
-}
+client.get('/clients', getclients)
+client.post('/clients',postclients)
+client.put('/clients', putClients)
